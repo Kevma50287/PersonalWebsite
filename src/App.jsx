72478536d27069d6243f4c22d0617f56d39e2832
Kevin <magavakevin10@gmail.com>
2022-11-23 17:41:11 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.scss";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Projects from "./components/projects/Projects";
-import { Canvas } from "react-three-fiber";
 import Banner from "./components/aboutMe/Banner";
 import AboutMe from "./components/aboutMe/AboutMe";
 
