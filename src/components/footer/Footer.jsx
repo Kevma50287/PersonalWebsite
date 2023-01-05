e1da172cff0a739c3ea74c0301c41ffc5f67d006
Kevin <magavakevin10@gmail.com>
2023-01-04 22:06:18 -0500

    updated doc

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <h1 className='gradient-highlight'>Contact Information</h1>
       <p>Email address: kevma50287@gmail.com / magavakevin10@gmail.com</p>
       <p>Phone Number: 347-935-7112</p>
-      <a href='src/assets/Kevin_Ma_Resume.pdf' download='Kevin_Ma_Resume.pdf'>Download Resume</a>
+      <a href='/documents/Kevin_Ma_Resume.pdf' download='Kevin_Ma_Resume.pdf'>Download Resume</a>
     </div>
   )
 }
