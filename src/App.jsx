0b15df834a6852b2c2673698fe56d49954369d33
Kevin <magavakevin10@gmail.com>
2023-01-04 22:18:49 -0500

    added title

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Header from "./components/header/Header";
 import Projects from "./components/projects/Projects";
 import Banner from "./components/aboutMe/Banner";
 import AboutMe from "./components/aboutMe/AboutMe";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title="Kevin Ma - Portfolio"
+  }, [])
+  
   return (
       <div className="App">
         <Header />
