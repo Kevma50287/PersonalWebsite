b7c8f9ffdebbf927b60472837525cc958fedac90
Kevin Ma <kevma50287@gmail.com>
2023-03-17 17:47:42 -0400

    Resolved aniamtion synchronization delay

---
+++ src/images.js
@@ -0,0 +1,9 @@
+import image1 from "../public/assets/images/pic1.jpg"
+import image2 from "../public/assets/images/pic2.jpg"
+import image3 from "../public/assets/images/pic3.jpg"
+
+const imagesArray = [
+  image1, image2, image3
+]
+
+export default imagesArray
